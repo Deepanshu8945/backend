42981739c3683694d47a7b335c7f45b98fc94968
Deepanshu8945 <Deepanshu010101@gmail.com>
2025-01-19 10:15:34 +0530

    utils

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,21 +1,8 @@
-class ApiError extends Error {
-    constructor(
-        statusCode,
-        message= "Something went wrong",
-        errors = [],
-        statck = ""
-    ){
-        super(message)
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
         this.statusCode = statusCode
-        this.data = null
-        this.message = message
-        this.success = false;
-        this.errors = errors
-        if (statck) {
-            this.stack = statck
-        } else{
-            Error.captureStackTrace(this, this.constructor)
-        }
+        this.data = data
+        this,message = message
+        this.success = statusCode < 400
     }
 }
-export {ApiError}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,6 +7,7 @@ export { asyncHandler };
 // const asyncHandler = () => {}
 // const asyncHandler = (func) => () => {}
 // const asyncHandler = (func) => async () => {}
+  
 // const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
 //         await fn(req, res, next)
