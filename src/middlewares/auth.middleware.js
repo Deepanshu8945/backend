e27a0a189038f143faee3511831652ec68b0a786
Deepanshu8945 <Deepanshu010101@gmail.com>
2025-02-02 18:59:42 +0530

    Fix username and email validation in loginUser

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,13 @@
-import { asyncHandler } from "../utils/asyncHandler"
-import { ApiError } from "../utils/ApiError"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
 import jwt, { decode } from "jsonwebtoken"
-import { User } from "../models/user.model"
+import { User } from "../models/user.model.js"
 
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
+        
+        
         if(!token){
             throw new ApiError(401,"Unauthorized request")
         }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
-
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 const router = Router();
 
 router.route("/register").post(
@@ -23,7 +23,5 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 
-//testing 
-router.route("/test").post(testFunction)
 
 export default router
